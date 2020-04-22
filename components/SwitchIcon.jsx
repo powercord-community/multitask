8cab1933e82086654233670f7ee82c822bf3b35a
Bowser65 <bowoser@weeb.services>
2020-04-22 03:44:55 +0200

    License change
    OSL is like AGPL but without the linking bullshit
    Fuck stallman

--- components/SwitchIcon.jsx
+++ components/SwitchIcon.jsx
@@ -1,19 +1,6 @@
-/**
- * Multitask, a Powercord plugin made to enhance productivity
- * Copyright (C) 2019 Bowser65
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+/*
+ * Copyright (c) 2020 Bowser65
+ * Licensed under the Open Software License version 3.0
  */
 
 const { React } = require('powercord/webpack');
